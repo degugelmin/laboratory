21685496b30cd6b4183f6509d87f22ec589459d0
Denise Gugelmin <denisegug@gmail.com>
2021-03-10 23:50:32 -0300

    Improving logic to verify credit card issue.

--- angular10-lab/src/app/shared/validators/issuer-identifier.validator.ts
+++ angular10-lab/src/app/shared/validators/issuer-identifier.validator.ts
@@ -1,26 +1,34 @@
 import { AbstractControl, ControlContainer } from '@angular/forms';
 
 export function issuerValidator(control: AbstractControl) {
-    if (control.value.length >= 1 && control.value.length < 7) {
-        if (control.value == '4') {
-            console.log('Visa');
-        } else if (
-            control.value == '51' || 
-            control.value == '52' || 
-            control.value == '53' ||
-            control.value == '54' ||
-            control.value == '55' ||
-            control.value == '222100' ||
-            control.value == '272099') {
-                console.log('Mastercard');
-        } else if (control.value == '34' || control.value == '37') {
-            console.log('American Express');
-        }
 
-        // Improve this logic to verify the card numbers that starts with, and not equal these values
-        // Improve this logic to get range and not fixed values (Mastercard 51 to 55)
-        // Implement Discover - 6011, 622126 to 622925, 644, 645, 646, 647, 648, 649, 65
-        // Implement Diners Club International - 36
-        // Implement JCB - 3528 to 3589
-    }   
+    if (control.value.startsWith('4')) {
+            console.log('Visa');
+    } else if (
+            control.value.startsWith('51') ||
+            control.value.startsWith('52') ||
+            control.value.startsWith('53') ||
+            control.value.startsWith('54') ||
+            control.value.startsWith('55') ||
+            control.value.startsWith('222100') ||
+            control.value.startsWith('272099')) {
+        console.log('Mastercard');
+    } else if (control.value.startsWith('34') ||
+                control.value.startsWith('37')) {
+        console.log('American Express');
+    } else if (control.value.startsWith('6011') ||
+                //between('622126','622925') ||
+                control.value.startsWith('644') ||
+                control.value.startsWith('645') ||
+                control.value.startsWith('646') ||
+                control.value.startsWith('647') ||
+                control.value.startsWith('648') ||
+                control.value.startsWith('649') ||
+                control.value.startsWith('65')) {
+        console.log('Discover');
+    } else if (control.value.startsWith('36')) {
+        console.log('Diners Club International');
+    } // Implement JCB - 3528 to 3589
 }
+        // Improve this logic to get range and not fixed values (Mastercard 51 to 55)
+        
